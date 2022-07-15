638d324974b19be212d1309f681985a1a1abbf7c
Luan Rafael <luanraffaell@hotmail.com>
2022-07-15 09:46:31 -0300

    Toast fix1

--- frontend/src/components/notificationButton/index.tsx
+++ frontend/src/components/notificationButton/index.tsx
@@ -1,5 +1,6 @@
 import axios from 'axios';
 import { toast } from 'react-toastify';
+
 import icon from '../../assets/img/notification-icon.svg'
 import { BASE_URL } from '../../utils/request';
 import './styles.css'
@@ -8,8 +9,8 @@ type Props = {
     saleId: number;
 }
 
-function handleClick(saleId : number){
-    axios(`${BASE_URL}/sales/${saleId}/notification`)
+function handleClick(id : number){
+    axios(`${BASE_URL}/sales/${id}/notification`)
     .then(response => {
         toast.info("SMS enviado com sucesso")
     })
