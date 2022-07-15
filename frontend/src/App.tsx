3a9a6ccfdf9ab2f2395e29c7d1b02e5cdbb90e40
Luan Rafael <luanraffaell@hotmail.com>
2022-07-15 11:46:42 -0300

    toasContainer fix

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,6 +3,7 @@ import { ToastContainer } from 'react-toastify';
 import Header from "./components/Header"
 import SalesCard from "./components/SalesCard"
 import 'react-toastify/dist/ReactToastify.min.css';
+
 function App() {
   return (
     <>
@@ -12,10 +13,11 @@ function App() {
         <section id="sales">
           <div className="dsmeta-container">
             <SalesCard />
+            <ToastContainer />
           </div>
         </section>
       </main>
-      <ToastContainer />
+      
     </>
   )
 }
